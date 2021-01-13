b53494e73186446ad6187e6e6f5ccd2d8cbb2103
mac <mac@XiaodanZhou-Air.local>
2021-01-13 01:14:08 -0800

    vis

--- scr/Vis.PMbySate.R
+++ scr/Vis.PMbySate.R
@@ -1,27 +1,29 @@
 setwd("/Users/mac/Documents/GitHub/covid_wildfire")
 source("scr/Utilities.R")
 library(scales) 
-
+library(latex2exp)
 df = load.data()
-df = df[df$date >= "2020-03-15", ]
-df = df[df$date <= "2020-09-24", ]
 df$weeks = cut(df$date, breaks="week")
+df = df[df$date >= ymd("2020-03-16"), ]
 
 ### by week, log 
 x.lab = unique(reorder(df$weeks,df$date))
 x.lab = paste0(month(x.lab), "/", day(x.lab))
+x.lab[!((1:length(x.lab))%%2)] = ""
 p0 = ggplot(data=df) + 
   geom_boxplot(aes(x=reorder(weeks,date),y=pm25), outlier.shape = NA) + 
-  facet_wrap(. ~ state.x, scales='free', ncol = 1) + theme_bw() +
+  facet_wrap(. ~ state, scales='free', ncol = 1) + 
+  theme_bw() +
   scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) + 
   scale_y_log10(limits = c(1,1e3)) +
-  ylab("PM2.5") + xlab("Date") + 
-  scale_x_discrete(labels = x.lab)# + 
-  # theme(strip.text = element_text(size = 11))
+  scale_x_discrete(labels = x.lab) + 
+  ylab(TeX("$PM_{2.5}$ ($\\mu g/m^3$ log scaled)")) + 
+  xlab("Date")
 
-file.pdf = "output/Vis.PMbySate.pdf"
-pdf(file.pdf, width = 10, height = 6)
+file.pdf = "output/pm_by_states.pdf"
+pdf(file.pdf, width = 10, height = 8)
+# pdf(file.pdf, width = 8, height = 6) #v2
 do.call('grid.arrange',c(list(p0), top = ""))
 dev.off()
 
@@ -29,11 +31,3 @@ dev.off()
 # ggplot(data=df) + 
 #   geom_boxplot(aes(x=as.factor(date),y=log(pm25+.1)),outlier.shape = NA) + 
 #   facet_wrap(. ~ state.x, ncol = 1) + theme_bw()
-
-### by week, raw 
-# ggplot(data=df) + 
-#   geom_boxplot(aes(x=format(date, "%Y/%W"),y=pm25),outlier.shape = NA) + 
-#   facet_wrap(. ~ state.x, ncol = 1) + theme_bw()
-
-
-
--- scr/Vis.pm&cases&deaths.top10.R
+++ scr/Vis.pm&cases&deaths.top10.R
@@ -1,15 +1,16 @@
 # Exploratory Data Analysis 
 # function: visualize the PM2.5 and case count and death count in pairs
+# https://stackoverflow.com/questions/13649473/add-a-common-legend-for-combined-ggplots 
 
 setwd("/Users/mac/Documents/GitHub/covid_wildfire")
 source("scr/Utilities.R")
 df = load.data()
-df = df[df$date >= "2020-03-15", ]
-df = df[df$date <= "2020-09-24", ]
+## PM2.5 (blue, ug/m3), COVID-19 incidences (red) and deaths (black) of the most populated counties (FIPS)
+
 ########################### top counties ################################
 ### set up 
 n.col.grid = 6
-file.name = paste0("output/pm&cases&deaths.top10z.pdf")
+file.name = paste0("output/pm&cases&deaths.top10.Jan2021_.pdf")
 fips.unique = unique(df$FIPS[order(df$population, decreasing=TRUE)])[1:10]
 
 ### TODO ???????????????????
@@ -17,14 +18,6 @@ df$start.hazard = as.Date("2020-03-15")
 df$end.hazard = as.Date("2020-03-15")
 df$start.hazard[!is.na(df$pmhazard)&(df$pmhazard > 0)] = df$date[!is.na(df$pmhazard)&(df$pmhazard > 0)] - 1
 df$end.hazard[!is.na(df$pmhazard)&(df$pmhazard > 0)] = df$date[!is.na(df$pmhazard)&(df$pmhazard > 0)] + 1
-
-
-# g_legend<-function(a.gplot){
-#   tmp <- ggplot_gtable(ggplot_build(a.gplot))
-#   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
-#   legend <- tmp$grobs[[leg]]
-#   return(legend)}
-
 
 plot.list = list()
 iplot = 1
@@ -43,9 +36,9 @@ for (ifips in fips.unique) {
           # axis.text = element_text(size = 11),
           axis.title.x = element_blank(),
           axis.title.y = element_blank(), 
-          plot.title = element_text(hjust = 0.5)) + 
+          plot.title = element_text(hjust = 0.5, size = 8)) +  # , face = "bold" 
     scale_color_manual(name="", values=c(PM25="blue")) +
-    xlab("Date") + ggtitle(ifips)
+    xlab("Date") + ggtitle(paste0(df.selected$County[1], " County, ", df.selected$state[1]))
  
   # if (iplot == 1) mylegend1 = g_legend(p1)
   p1 = p1 + theme(legend.position = "none")
@@ -69,9 +62,9 @@ for (ifips in fips.unique) {
           axis.title.x = element_blank(),  
           axis.title.y = element_blank(),  
           # legend.position = "none",
-          plot.title = element_text(hjust = 0.5)) + 
+          plot.title = element_text(hjust = 0.5, size = 8)) +
     scale_color_manual(name="", values=c(Cases="red")) +
-    xlab("Date") + ggtitle("")
+    xlab("Date") + ggtitle(" ")
   
   # if (iplot == 2) mylegend2 = g_legend(p2)
   p2 = p2 + theme(legend.position = "none")
@@ -95,7 +88,7 @@ for (ifips in fips.unique) {
           # legend.position = "none",
           axis.title.x = element_blank(),  
           axis.title.y = element_blank(),  
-          plot.title = element_text(hjust = 0.5)) + 
+          plot.title = element_text(hjust = 0.5, size = 8)) +
     scale_color_manual(name="", values=c(Deaths="black")) +
     xlab("Date") + ggtitle("")
   # if (iplot == 3) mylegend3 = g_legend(p3)
@@ -108,49 +101,9 @@ for (ifips in fips.unique) {
   plot.list[[iplot]] = p3
   iplot = iplot + 1
 }
-# mylegend1, mylegend2, mylegend3
-# https://stackoverflow.com/questions/13649473/add-a-common-legend-for-combined-ggplots 
 pdf(file.name, width = 10, height = 8)
 do.call('grid.arrange',c(plot.list, ncol = n.col.grid, 
-                         top = "PM2.5 (blue, ug/m3), COVID-19 incidences (red) and deaths (black) of the most populated counties (FIPS)",
+                         top = "",
                          bottom="", left=""))
 dev.off()
 
-
-########################### all counties ################################
-# ### set up 
-# n.col.grid = 8
-# file.name = paste0("ExploratoryDataAnalysis/pm&cases_raw_moddat2.pdf")
-# 
-# fips.unique = unique(df$FIPS[order(df$population, decreasing=TRUE)])
-# 
-# pdf(file.name, width = 12, height = 200/n.col.grid)
-# 
-# plot.list = list()
-# for (ifips in 1:length(fips.unique)) {
-# 
-#   iplot = (ifips-1)*2+1
-#   
-#   df.selected = df[df$FIPS == fips.unique[ifips], ]
-#   
-#   ### visualize PM2.5
-#   p1 = ggplot() +
-#     geom_point(data=df.selected, aes(x=date, y=pm25), colour="blue", size = .1) +
-#     theme(legend.position="none", axis.title.x=element_blank(),
-#           axis.text.x=element_blank(), axis.ticks.x=element_blank(), 
-#           axis.text.y=element_blank(), axis.ticks.y=element_blank())# + 
-#     # ggtitle(as.character(fips.unique[ifips]))
-#   
-#   ### visualize number of cases
-#   p2 = ggplot() +
-#     geom_point(data=df.selected, aes(x=date, y=cases), colour="red", size = .1) +
-#     theme(legend.position="none", axis.title.x=element_blank(),
-#           axis.text.x=element_blank(), axis.ticks.x=element_blank(), 
-#           axis.text.y=element_blank(), axis.ticks.y=element_blank())
-# 
-#   plot.list[[iplot]] = p1
-#   plot.list[[iplot+1]] = p2
-# 
-# }
-# do.call('grid.arrange',c(plot.list, ncol = n.col.grid, top = "Raw Value of PM2.5 case count"))
-# dev.off()
