166143b4e760c798c1dc5c464ba28992a471730d
mac <mac@XiaodanZhou-Air.local>
2021-02-07 16:43:32 -0800

    summary data

--- scr/SummarizeData.R
+++ scr/SummarizeData.R
@@ -4,6 +4,8 @@ setwd(project.dir)
 source("scr/Utilities.R")
 dff = load.data()
 
+TOTAL.POP = 51344853
+
 ####################### statistics across three states ###############################
 ### number of counties in each state
 for (istate in unique(dff$State))
@@ -17,7 +19,7 @@ max(dff$date) - min(dff$date) + 1
 total.pop = 0
 for (ifips in unique(dff$FIPS)) {
   total.pop = total.pop + dff$population[dff$FIPS == ifips][1] }
-print(paste("population coverage", round(total.pop / 51344853 * 100, 3), "%"))
+print(paste("population coverage", round(total.pop / TOTAL.POP * 100, 3), "%"))
 
 ### missing in pm2.5 2020 been replaced by historical value
 # statistics for these 250 replacement 
@@ -37,33 +39,9 @@ sum(is.na(dff$deaths)) / dim(dff)[1] * 100
 ### the number of counties with missing mobility data 
 sum(is.na(dff$relative_change_feb)) / dim(dff)[1]
 
-######################## Table: #####################################
-### number of wildfire days by county
-subset = dff[!is.na(dff$hazardmap), ]
-temp = data.frame(subset %>% group_by(FIPS) %>% summarise(count=sum(hazardmap==27)))
-paste(sum(temp$count), "wildfire days in the analysis/")
-summary(temp$count)
-summary(temp$count) / 277
-
-### total cases and deaths
-paste(sum(dff$cases, na.rm=T), "cases")
-paste(sum(dff$deaths, na.rm=T), "deaths")
-
-### percentage of cases and deaths during wildfire days 
-case_fire = sum(dff$cases[dff$wildfire==T], na.rm=T)
-death_fire = sum(dff$deaths[dff$wildfire==T], na.rm=T)
-
-case_pct = round(case_fire / sum(dff$cases, na.rm=T) * 100)
-death_pct = round(death_fire / sum(dff$deaths, na.rm=T) * 100)
-
-paste(case_fire, "cases during wildfire (", case_pct, "%)")
-paste(death_fire, "deaths during wildfire (", death_pct, "%)")
-
-### pm2.5 in wildfire days
-summary(dff$pm25)
-summary(dff$pm25[dff$wildfire==T])
-summary(dff$pm25[dff$wildfire==F])
-# summary(dff$pm25[(dff$wildfire==F)&(dff$date<="2020-11-26")])
+### the 6 counties with missing in mobility
+c6 = unique(dff$FIPS[is.na(dff$relative_change_feb)])
+sum(unique(dff$population[dff$FIPS %in% c6])) / TOTAL.POP * 100
 
 ######################## Table: statistics by states #####################################
 ### cases summary
@@ -143,4 +121,39 @@ print(paste0("Total & ",
              round(quantile(subset$pm25, .25, na.rm=T), 1), " - ", 
              round(quantile(subset$pm25, .75, na.rm=T), 1), ")"))
 
+
+
+######################## Table: #####################################
+### number of wildfire days by county
+subset = dff[!is.na(dff$hazardmap), ]
+temp = data.frame(subset %>% group_by(FIPS) %>% summarise(count=sum(hazardmap==27)))
+paste(sum(temp$count), "wildfire days in the analysis/")
+summary(temp$count)
+summary(temp$count) / 277
+
+### total cases and deaths
+paste(sum(dff$cases, na.rm=T), "cases")
+paste(sum(dff$deaths, na.rm=T), "deaths")
+
+### percentage of cases and deaths during wildfire days 
+case_fire = sum(dff$cases[dff$wildfire==T], na.rm=T)
+death_fire = sum(dff$deaths[dff$wildfire==T], na.rm=T)
+
+case_pct = round(case_fire / sum(dff$cases, na.rm=T) * 100)
+death_pct = round(death_fire / sum(dff$deaths, na.rm=T) * 100)
+
+paste(case_fire, "cases during wildfire (", case_pct, "%)")
+paste(death_fire, "deaths during wildfire (", death_pct, "%)")
+
+### pm2.5 in wildfire days
+summary(dff$pm25)
+summary(dff$pm25[dff$wildfire==T])
+summary(dff$pm25[dff$wildfire==F])
+# summary(dff$pm25[(dff$wildfire==F)&(dff$date<="2020-11-26")])
+
 #############################################################
+### pm2.5 the ambient level in wildfire days
+summary(dff$pm25[(dff$wildfire==T)])
+summary(dff$pm_wildfire[(dff$wildfire==T)])
+summary(dff$pm_ambient[(dff$wildfire==T)])
+summary(dff$pm25[(dff$wildfire==F)])
